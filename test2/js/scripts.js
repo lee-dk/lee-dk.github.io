dca281dc7c17bf4493d283e5c4d60c4d18f42ba4
DongGyu Lee <dlehdrb1129@gmail.com>
2020-08-21 12:09:10 +0900

    update homepage Bootstrap

--- test2/js/scripts.js
+++ test2/js/scripts.js
@@ -1,4 +1,9 @@
-(function ($) {
+/*!
+    * Start Bootstrap - Grayscale v6.0.2 (https://startbootstrap.com/themes/grayscale)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+    */
+    (function ($) {
     "use strict"; // Start of use strict
 
     // Smooth scrolling using jQuery easing
@@ -15,7 +20,7 @@
             if (target.length) {
                 $("html, body").animate(
                     {
-                        scrollTop: target.offset().top,
+                        scrollTop: target.offset().top - 70,
                     },
                     1000,
                     "easeInOutExpo"
@@ -32,6 +37,20 @@
 
     // Activate scrollspy to add active class to navbar items on scroll
     $("body").scrollspy({
-        target: "#sideNav",
+        target: "#mainNav",
+        offset: 100,
     });
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
